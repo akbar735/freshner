cf297da67093d19524a6d8a3c8dcc614aa6c3225
akbar735 <mdaliakbar858@gmail.com>
2024-04-07 01:24:51 +0530

    add recent media feature

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import AudioGallery from "./pages/audio_gallery/AudioGallery";
 import VideoGallery from "./pages/video_gallery/VideoGallery";
 import Settings from "./pages/settings/Settings";
 import { useAppSelector } from "./hooks";
+import PlayLists from "./pages/play_lists/PlayLists";
 App.displayName = 'App';
 export default function App(){
     const state = useAppSelector(state => state)
@@ -19,6 +20,7 @@ export default function App(){
                         <Route index element={<Home />} />
                         <Route path={pagePath.AUDIOGALLERY} element={<AudioGallery />} />
                         <Route path={pagePath.VIDEOGALLERY} element={<VideoGallery />} />
+                        <Route path={pagePath.PLAYLIST} element={<PlayLists />} />
                         <Route path={pagePath.SETTINGS} element={<Settings />} />
                     </Route>
                 </Routes>
